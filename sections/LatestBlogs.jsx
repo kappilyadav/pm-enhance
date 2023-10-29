e89a74711f24059845239e74002685bb31d68f4a
kapil yadav <ky22379@gmail.com>
2023-10-29 09:03:55 +0530

    button link update

--- sections/LatestBlogs.jsx
+++ sections/LatestBlogs.jsx
@@ -2,6 +2,8 @@
 import Link from "next/link"
 import LatestBlogCard from "../components/LatestBlogCard"
 import { articles } from "../constants"
+import Button from "@/components/Button"
+
 
 
 const LatestBlogs = () => {
@@ -13,6 +15,8 @@ const LatestBlogs = () => {
                 <h2 className="text-4xl font-palanquin font-bold text-black">Our <span className="text-gray-600">Latest</span> Articles</h2>
                 <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">Experience top-notch quality and style with our sought-after selections. Discover a world of comfort, design, and value.</p>
             </div>
+
+            <div className="lg:mt-11 mt-6"><Link href="/articles"><Button label="Explore All" backgroundColor={"bg-black"} textColor={"text-white"} /></Link></div>
 
             <div className="mt-16 grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-16">
                 {articles.map((article) => (
--- sections/PopularCourses.jsx
+++ sections/PopularCourses.jsx
@@ -2,6 +2,8 @@
 import Link from "next/link"
 import PopularCourseCard from "../components/PopularCourseCard"
 import { products } from "../constants"
+import Button from "@/components/Button"
+
 
 
 const PopularCourses = () => {
@@ -13,6 +15,8 @@ const PopularCourses = () => {
                 <h2 className="text-4xl font-palanquin font-bold text-black">Our <span className="text-gray-600">Best-selling</span> Courses</h2>
                 <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">Experience top-notch quality and style with our sought-after selections. Discover a world of comfort, design, and value.</p>
             </div>
+
+            <div className="lg:mt-11 mt-6"><Link href="/courses"><Button label="Explore All" backgroundColor={"bg-black"} textColor={"text-white"} /></Link></div>
 
             <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
                 {products.map((product) => (
